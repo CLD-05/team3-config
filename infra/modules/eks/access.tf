4aba5bb9e77a0d61f42adce627740f66f1880637
bovo22 <bvojn02@naver.com>
2026-05-26 15:31:36 +0900

    feat: GitHub Actions 권한을 EditPolicy로 하향 조정

--- infra/modules/eks/access.tf
+++ infra/modules/eks/access.tf
@@ -39,7 +39,7 @@ resource "aws_eks_access_policy_association" "github_actions" {
 
   cluster_name  = aws_eks_cluster.this.name
   principal_arn = var.github_actions_role_arn
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSEditPolicy"
 
   access_scope {
     type = "cluster"
