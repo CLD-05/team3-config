4d4f6a90cc9db1f3da33cffe304f313e04a5929a
bovo22 <bvojn02@naver.com>
2026-05-26 15:34:00 +0900

    feat:불필요한 파일 중복 삭제(outputs, variables 파일로 통합)

--- infra/modules/eks/variables.tf
+++ infra/modules/eks/variables.tf
@@ -33,3 +33,15 @@ variable "node_max_size" {
   type    = number
   default = 2
 }
+
+variable "admin_user_arns" {
+  description = "EKS 클러스터에 admin 권한을 가질 IAM User ARN 목록"
+  type        = list(string)
+  default     = []
+}
+
+variable "github_actions_role_arn" {
+  description = "GitHub Actions가 사용할 IAM Role ARN (CD용)"
+  type        = string
+  default     = ""
+}
