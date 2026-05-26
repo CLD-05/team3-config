33faadffebfc3ad16bccef7c1d2c0a9e1068056e
bovo22 <bvojn02@naver.com>
2026-05-26 12:30:08 +0900

    feat(infra): change terraform backend state file key path to project2

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tfstate-lionkdt5-team3"
-    key            = "infra/terraform.tfstate"
+    key            = "project2/terraform.tfstate"
     region         = "ap-northeast-2"
     dynamodb_table = "tfstate-lock-team3"
     encrypt        = true
