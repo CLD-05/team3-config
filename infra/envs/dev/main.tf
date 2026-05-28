3cbd29c20006147afd295734db32f1c683377851
chatnip <commcencer@gmail.com>
2026-05-28 16:42:55 +0900

    fix: s3 public

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -87,3 +87,27 @@ resource "aws_s3_bucket_cors_configuration" "app_storage" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "app_storage" {
+  bucket = aws_s3_bucket.app_storage.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "app_storage" {
+  bucket     = aws_s3_bucket.app_storage.id
+  depends_on = [aws_s3_bucket_public_access_block.app_storage]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.app_storage.arn}/*"
+    }]
+  })
+}
