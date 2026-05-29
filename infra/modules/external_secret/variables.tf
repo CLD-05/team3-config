0ff8520105382e83766338a293127331ded63b7d
KK-Hyeok <poro2705@gmail.com>
2026-05-29 14:36:28 +0900

    feat:external_secret_module

--- infra/modules/external_secret/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "external_secret_name" { type = string }
-variable "namespace" { type = string }
-variable "secret_store_name" { type = string }
-variable "target_k8s_secret_name" { type = string }
-variable "k8s_secret_key" { type = string }
-variable "aws_secretsmanager_key" { type = string }
-variable "aws_secret_property" { type = string }
